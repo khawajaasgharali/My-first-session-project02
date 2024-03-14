c6773bec638254d2bb3434590b27d459a5408157
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-14 18:15:16 +0500

    I have solved question #2  out of 45 .
    IT MY FIRST PIECE OF ASSIGNMENT THAT I SOLVED,.

---
+++ question2.js
@@ -0,0 +1,7 @@
+// we have took here "personName"as a variable//
+//"srting" here showing the type .//
+//and name in double quotions showing the value.//
+// it a total formula ...variable , type , value.//
+var personName = "khawaja asghar ali";
+console.log("HELLO," + " " + personName + " " + "would you like to learn sone python taday?");
+//  ALHAMDULILLAH I HAVE DONE 2ND ASSIGNMENT TODAY , BY MYSELF. 10/03/2024 ON SUNDAY//

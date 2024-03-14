a20dd04c6b6b3f08450114049f1521fa54da1f0a
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-15 00:55:16 +0500

    QUESTION 5 soved out of 45
    it was the repeated assignments from questions 4 .we just change the variable names.

---
+++ question5.js
@@ -0,0 +1,3 @@
+var person_name = "ALBERT EINSTIEN,";
+var message = "A person who never made a mistake never tried anything new.";
+console.log(person_name + " " + "once said," + " " + message);
---
+++ question5.ts
@@ -0,0 +1,4 @@
+let person_name:string = "ALBERT EINSTIEN,"
+let message:string = "A person who never made a mistake never tried anything new.";
+console.log(person_name +" "+ "once said," +" "+ message );
+// shukar alhamdulillah i have solved fifth one .//

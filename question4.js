00dfa8ebcc4c2eaf15c17cbf976024c1b3b0258e
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-15 00:53:08 +0500

    i have solved question4 out of 45
    in this assignment we had to make two varible respectively  with the name ,""qoute,"auther" and in quote we type compelete message from auther he said.that is called "quote" as a string.and 2nd  variable was "auther" inwhich value was putting his name ALBERT EINSTIEN.

---
+++ question4.js
@@ -0,0 +1,8 @@
+"use strict";
+let quote = "A person who never made a mistake never tried anything new.";
+// first we took here a complete "quote"(quote is as a variable)
+let auther = "ALBERT EINTIEN,";
+// here me mentioned auther as variable and albert is value put in variable.//
+console.log(auther + " " + " once said," + " " + quote);
+// for extra wording we use double quotation when we use the final command//
+// alhamdulillah question 4 have been solved by understanding method . 

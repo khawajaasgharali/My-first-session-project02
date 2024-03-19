7d8e76b9cfcb581a9d95b74175644fe1a40fbd01
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-19 21:02:24 +0500

    question 10 solved out of 45
    solved

---
+++ question10.js
@@ -0,0 +1,17 @@
+// 
+var sum1 = 5;
+var num2 = 3;
+console.log(5 + 3);
+var num3 = 12;
+var num4 = 4;
+console.log(12 - 4);
+var num5 = 4;
+var num6 = 2;
+console.log(4 * 2);
+var num7 = 16;
+var num8 = 2;
+console.log(16 / 2);
+// console.log(5 + 3);
+// console.log(11 - 3);
+// console.log(2 * 4);
+// console.log(16 / 2);

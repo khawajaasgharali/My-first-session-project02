2e2817de0b8be2e34ad359b959cb58b342420f1b
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-19 16:39:58 +0500

    we have made a simple calculator with typescript.
    AMONG THE FROFESSIONAL TEACHERS AND SPECIALLY SIR ZIA WHO IS LEADING WELL FROM THE VERY BASIC  LEVEL AND WE ARE UNDERSTADING WELL AND. LEARNING WELL.

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{message:"EnterfirstNumber", type:"number", name: "firstNumber"},
+
+{message:"entersecondNumber", type:"number", name: "secondNumber"},
+
+{message:"select one of the operators to perform operation", type: "list", name: "operator",choices:["Addition", "Subtraction", "Multiplication", "Division"],},]);
+
+// if (answer.operator === "Additon"){console.log("your value is:" + answer.firstNumber + answer.secondNumber);}
+
+// if (answer.operator === "Additon"){console.log( answer.firstNumber + answer.secondNumber);}
+
+//                   NOW USING THE SECOND AND IMPORTANT PART  //    "CONDITIONAL STATEMENT"  //
+
+//                                 WITH IF , ALSE IF AND ESLE CONDITONS                      //
+
+if (answer.operator === "Addition"){console.log(answer.firstNumber + answer.secondNumber)}
+
+else if (answer.operator === "Subtraction"){console.log(answer.firstNumber - answer.secondNumber)}
+
+else if (answer.operator === "Multiplication"){console.log(answer.firstNumber * answer.secondNumber)}
+
+else if (answer.operator === "Division"){console.log(answer.firstNumber / answer.secondNumber)}
+
+else {console.log("PLEASE SELECT THE VALID OPERATOR")}
+
+

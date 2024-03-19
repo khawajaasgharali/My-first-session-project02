91bc227db209b0ed491237c60ffd33ebc074c753
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-19 21:01:33 +0500

    question no.9 completed
    solved

---
+++ question9.js
@@ -0,0 +1,6 @@
+"use strict";
+//       FAVORITE NUMBER IN VARIABLE   IN BY PRINTING  MESSAGE    /////
+let favoriteNumber = 7;
+//  and we mention by message that my favorite number is 7  //
+let message = "MY FAVORITE MUMVER IS";
+console.log(message, favoriteNumber);
---
+++ question9.ts
@@ -0,0 +1,5 @@
+//       FAVORITE NUMBER IN VARIABLE   IN BY PRINTING  MESSAGE    /////
+let favoriteNumber:number = 7;
+//  and we mention by message that my favorite number is 7  //
+let message:string = "MY FAVORITE MUMVER IS";
+console.log(message , favoriteNumber);

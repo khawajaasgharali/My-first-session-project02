f785bf5426a71cdc4b8791cec8829bbcffcc4e9b
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-19 21:00:38 +0500

    question no.7 & 8 completed
    solved

---
+++ question7&8.ts
@@ -0,0 +1,19 @@
+let sum1 = 5;
+let num2 = 3;
+console.log(5 + 3);
+let num3 = 12;
+let num4 = 4;
+console.log(12 - 4);
+let num5 = 4;
+let num6 = 2;
+console.log(4 * 2);
+let num7 = 16;
+let num8 = 2;
+console.log(16 / 2);
+alhamdulillah i have done question 7 and 8........
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(2 * 4);
+console.log(16 / 2);
+
+

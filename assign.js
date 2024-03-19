74642b89cfe0902609e67370461c5dd439758119
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-19 21:34:56 +0500

    COMPERISION OPERATORS
    SOLVED COMPERISION OPERATORS

---
+++ assign.js
@@ -0,0 +1,17 @@
+//  ( ASSINGMENT  OPERATORS    )   //
+// we will learn about assignment opetators//we assign the value//
+var z = 20;
+var z = 10;
+console.log(z);
+var z = 10;
+var z = 10 + 1;
+console.log(z);
+var z = 10;
+var z = 10 - 1;
+console.log(z);
+var z = 10;
+var z = 10 * 1;
+console.log(z);
+var z = 10;
+var z = 10 / 5;
+console.log(z);
---
+++ assign.ts
@@ -0,0 +1,22 @@
+                //  ( ASSINGMENT  OPERATORS    )   //
+// we will learn about assignment opetators//we assign the value//
+let z: number = 20;
+let z = 10;
+console.log(z);
+
+let z:number = 10;
+let z = 10 + 1;
+console.log(z);
+
+let z:number = 10;
+let z = 10 - 1;
+console.log(z);
+
+let z:number = 10;
+let z = 10 * 1;
+console.log(z);
+
+let z:number = 10;
+let z = 10 / 5;
+console.log(z);
+// ANSER WILL BE 10 ,11 ,9 10 2. RESPECTIVLY

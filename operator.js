4edbe8c19fced4e5a77e1bc6ca4bb1a69ee1543a
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-19 21:12:35 +0500

    EIRTHMETIC OPERATOR
    WHERE WE MAKE .ADDITION ,MULTIPLICATION, SUBTRACTION,AND DIVISION

---
+++ operator.js
@@ -0,0 +1,23 @@
+// now today we learn various type of operators//
+// first we are going to learn airthmetic operators//
+// like + , - , / , * . //frist + operator//
+var X = 10;
+var result = 10;
+console.log(X);
+// let a value of x =  10 is given in all parameters you have make changes with this value accordiong to the situation//
+// now we will make here plus trasaction //
+var x = 10;
+var result = x + 5;
+console.log(result);
+// now we will make here minus trasaction //
+var x = 10;
+var result = x - 5;
+console.log(result);
+// now we will make here divide trasaction //
+var X = 10;
+var result = x / 5;
+console.log(result);
+// now we will make here multiply trasaction //
+var X = 10;
+var result = x * 5;
+console.log(result);
---
+++ operator.ts
@@ -0,0 +1,25 @@
+// now today we learn various type of operators//
+// first we are going to learn airthmetic operators//
+// like + , - , / , * . //frist + operator//
+let x: number = 10;
+let result = 10;
+console.log(x);
+// let a value of x =  10 is given in all parameters you have make changes with this value accordiong to the situation//
+// now we will make here plus trasaction //
+let x: number = 10;
+let ans = x + 5;
+console.log(result);
+// // now we will make here minus trasaction //
+let x: number = 10;
+let result = x - 5;
+console.log(result);
+// // now we will make here divide trasaction //
+let X: number =10;
+let result = x / 5;
+console.log(result);
+// // now we will make here multiply trasaction //
+let X: number =10;
+let result = x * 5;
+console.log(result);
+// // these were all airthmetic operators.....//
+

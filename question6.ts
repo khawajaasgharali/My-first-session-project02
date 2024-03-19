db167844cb3244626caef50769ceb9b7da2b3faa
khawajaasgharali <160276945+khawajaasgharali@users.noreply.github.com>
2024-03-19 20:59:41 +0500

    question no .6 solved out of 45
    exercise 6 completed

---
+++ question6.ts
@@ -0,0 +1,6 @@
+// STRIPPING NAMES...
+let whitespaceName:string = " \n\t KHAWAJA ASGHAR ALI.\t\n";
+console.log(whitespaceName);
+let withoutwhitespaceName = whitespaceName.trim();
+console.log(withoutwhitespaceName);
+// alhamdulillah sixth question have solved.after understanding.
